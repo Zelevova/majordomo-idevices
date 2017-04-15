d0a5b5d5406ad437d98580a91acfc861d91d4bdc
Zelevova <Zelevova@users.noreply.github.com>
2017-04-15 14:21:17 +0300

    Add files via upload

--- lib/idevices.php
+++ lib/idevices.php
@@ -1,11 +1,12 @@
 <?php
+
 function messageToApple($name, $message, $sound = true, $subject = "") {
   if($message == "") {
     return 0;
   }
   require_once(ROOT.'modules/idevices/FindMyiPhone.php');
   try {
-    $devices = SQLSelect("SELECT appleIDs.APPLEID, appleIDs.PASSWORD, idevices.DEVICE_ID FROM appleIDs, idevices WHERE appleIDs.APPLEID = idevices.APPLEID AND idevices.NAME =  '".$$
+    $devices = SQLSelect("SELECT appleIDs.APPLEID, appleIDs.PASSWORD, idevices.DEVICE_ID FROM appleIDs, idevices WHERE appleIDs.APPLEID = idevices.APPLEID AND idevices.NAME =  '".$name."'");
     foreach($devices as $device) {
       $FindMyiPhone = new FindMyiPhone($device['APPLEID'], $device['PASSWORD'], false);
       $FindMyiPhone->sendMessage($device['DEVICE_ID'], $message, (bool)$sound, $subject);
@@ -18,7 +19,7 @@ function messageToApple($name, $message, $sound = true, $subject = "") {
 function soundToApple($name, $subject = "") {
   require_once(ROOT.'modules/idevices/FindMyiPhone.php');
   try {
-    $devices = SQLSelect("SELECT appleIDs.APPLEID, appleIDs.PASSWORD, idevices.DEVICE_ID FROM appleIDs, idevices WHERE appleIDs.APPLEID = idevices.APPLEID AND idevices.NAME =  '".$$
+    $devices = SQLSelect("SELECT appleIDs.APPLEID, appleIDs.PASSWORD, idevices.DEVICE_ID FROM appleIDs, idevices WHERE appleIDs.APPLEID = idevices.APPLEID AND idevices.NAME =  '".$name."'");
     foreach($devices as $device) {
       $FindMyiPhone = new FindMyiPhone($device['APPLEID'], $device['PASSWORD'], false);
       $FindMyiPhone->playSound($device['DEVICE_ID'], $subject);
@@ -31,7 +32,7 @@ function soundToApple($name, $subject = "") {
 function lockToApple($name, $message, $phoneNumber = "") {
   require_once(ROOT.'modules/idevices/FindMyiPhone.php');
   try {
-    $devices = SQLSelect("SELECT appleIDs.APPLEID, appleIDs.PASSWORD, idevices.DEVICE_ID FROM appleIDs, idevices WHERE appleIDs.APPLEID = idevices.APPLEID AND idevices.NAME =  '".$$
+    $devices = SQLSelect("SELECT appleIDs.APPLEID, appleIDs.PASSWORD, idevices.DEVICE_ID FROM appleIDs, idevices WHERE appleIDs.APPLEID = idevices.APPLEID AND idevices.NAME =  '".$name."'");
     foreach($devices as $device) {
       $FindMyiPhone = new FindMyiPhone($device['APPLEID'], $device['PASSWORD'], false);
       $FindMyiPhone->lostMode($device['DEVICE_ID'], $message, $phoneNumber);
@@ -44,13 +45,14 @@ function lockToApple($name, $message, $phoneNumber = "") {
 function findApple($name, $timeout = 60) {
   require_once(ROOT.'modules/idevices/FindMyiPhone.php');
   try {
-    $devices = SQLSelect("SELECT appleIDs.APPLEID, appleIDs.PASSWORD, idevices.DEVICE_ID FROM appleIDs, idevices WHERE appleIDs.APPLEID = idevices.APPLEID AND idevices.NAME =  '".$$
+    $devices = SQLSelect("SELECT appleIDs.APPLEID, appleIDs.PASSWORD, idevices.DEVICE_ID FROM appleIDs, idevices WHERE appleIDs.APPLEID = idevices.APPLEID AND idevices.NAME =  '".$name."'");
     foreach($devices as $device) {
       $FindMyiPhone = new FindMyiPhone($device['APPLEID'], $device['PASSWORD'], false);
-      $FindMyiPhone->playSound($device['DEVICE_ID'], $subject);
+      //$FindMyiPhone->playSound($device['DEVICE_ID'], $subject);
     }
   } catch (exception $e) {
     registerError('idevices', 'ERROR: ' . $e->getMessage());
   }
 }
+
 ?>
