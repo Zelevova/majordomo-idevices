c806e588495bea259944d75ff6a52aabd72fee31
Zelevova <Zelevova@users.noreply.github.com>
2017-04-15 09:37:13 +0300

    Update idevices.php

--- lib/idevices.php
+++ lib/idevices.php
@@ -1,2 +1,31 @@
 <?php
+
+function messageToApple($name, $message, $sound = true, $subject = "") {
+  if($message == "") {
+		return 0;
+  }
+  require_once(ROOT.'modules/idevices/FindMyiPhone.php');
+	try {
+    $devices = SQLSelect('SELECT `appleIDs`.`APPLEID`, `appleIDs`.`PASSWORD`, `idevices`.`device_ID`
+FROM `appleIDs`, `idevices`
+WHERE `appleIDs`.`APPLEID` = `idevices`.`APPLEID`
+ AND `idevices`.`NAME` =  '.$name.'');
+    foreach($devices as $device)
+		$FindMyiPhone = new FindMyiPhone($username, $password, TRUE);
+		$FindMyiPhone->sendMessage($device_id, $message, (bool)$sound, $subject);
+	} catch (exception $e) {
+		echo 'ERROR: ' . $e->getMessage() ;
+	}
+}
+
+function soundToApple($device, $subject = "") {
+}
+
+function lockToApple($device, $message, $phoneNumber = "") {
+}
+
+function findApple($device, $timeout = 60) {
+  
+}
+
 ?>
