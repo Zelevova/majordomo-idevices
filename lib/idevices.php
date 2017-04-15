df6c05bd1ea82a1e3b23b4e5bcef96929c8785a8
Zelevova <Zelevova@users.noreply.github.com>
2017-04-15 09:39:39 +0300

    Update idevices.php

--- lib/idevices.php
+++ lib/idevices.php
@@ -11,8 +11,8 @@ FROM `appleIDs`, `idevices`
 WHERE `appleIDs`.`APPLEID` = `idevices`.`APPLEID`
  AND `idevices`.`NAME` =  '.$name.'');
     foreach($devices as $device)
-		$FindMyiPhone = new FindMyiPhone($username, $password, TRUE);
-		$FindMyiPhone->sendMessage($device_id, $message, (bool)$sound, $subject);
+		$FindMyiPhone = new FindMyiPhone($device['APPLEID'], $device['PASSWORD'], TRUE);
+		$FindMyiPhone->sendMessage($device['device_id'], $message, (bool)$sound, $subject);
 	} catch (exception $e) {
 		echo 'ERROR: ' . $e->getMessage() ;
 	}
