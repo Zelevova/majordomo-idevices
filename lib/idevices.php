8c52ee6cd63a67e882f9215715c6f9b8d7ebe993
Zelevova <Zelevova@users.noreply.github.com>
2017-04-17 03:41:26 +0300

    Update idevices.php

--- lib/idevices.php
+++ lib/idevices.php
@@ -49,14 +49,14 @@ function findApple($name, $timeout = 60) {
     foreach($devices as $device) {
       $FindMyiPhone = new FindMyiPhone($device['APPLEID'], $device['PASSWORD'], false);
       $location =  $FindMyiPhone->locate($device['DEVICE_ID']);
+      if($location->horizontalAccuracy > 1000)
+        $location =  $FindMyiPhone->locate($device['DEVICE_ID']);
       $prop=SQLSelectOne("SELECT * FROM idevices WHERE APPLEID='".DBSafe($device['APPLEID'])."' AND DEVICE_ID='".DBSafe($device['DEVICE_ID'])."'");
       $prop['NAME'] = $FindMyiPhone->devices[$device['DEVICE_ID']]->name;
       $prop['DEVICE_ID'] = $device['DEVICE_ID'];
       $prop['APPLEID'] = $device['APPLEID'];
       $prop['BATTERY_LEVEL'] = $FindMyiPhone->devices[$device['DEVICE_ID']]->batteryLevel*100;
       $prop['BATTERY_STATUS'] = ($FindMyiPhone->devices[$device['DEVICE_ID']]->batteryStatus == "NotCharging") ? 0 : 1;
-      if($location->horizontalAccuracy > 1000)
-        $location =  $FindMyiPhone->locate($device['DEVICE_ID']);
       $prop['ACCURACY'] = $location->horizontalAccuracy;
       $prop['LATITUDE'] = $location->latitude;
       $prop['LONGITUDE'] = $location->longitude;
