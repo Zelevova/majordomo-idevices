dfc74d5103e33fc81d49caa26e9453c1e9be83ee
Zelevova <Zelevova@users.noreply.github.com>
2017-04-17 17:27:42 +0300

    Update idevices.php
    Чистка кода

--- lib/idevices.php
+++ lib/idevices.php
@@ -62,8 +62,15 @@ function findApple($name, $timeout = 60) {
       $prop['LONGITUDE'] = $location->longitude;
       $prop['UPDATED']=date('Y-m-d H:i:s');
       SQLUpdateInsert('idevices', $prop);
-      require_once(ROOT.'modules/idevices/toGPS.php');
-      AppleToGPS($prop);
+      if(file_exists(DIR_MODULES.'app_gpstrack/installed')) {
+        $_REQUEST['deviceid']  = $prop['NAME'];
+        $_REQUEST['battlevel'] = $prop['BATTERY_LEVEL'];
+        $_REQUEST['charging'] = $prop['BATTERY_STATUS'];
+        $_REQUEST['latitude']  = $prop['LATITUDE'];
+        $_REQUEST['longitude'] = $prop['LONGITUDE'];
+        $_REQUEST['accuracy'] = $prop['ACCURACY'];
+        include_once('./gps.php');
+      }
     }
   } catch (exception $e) {
     registerError('idevices', 'ERROR: ' . $e->getMessage());
