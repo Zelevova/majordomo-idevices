bd6ec3096196a98e279bedd330bc127be7d5b75b
Zelevova <Zelevova@users.noreply.github.com>
2017-04-17 03:07:55 +0300

    Update idevices.php

--- lib/idevices.php
+++ lib/idevices.php
@@ -48,7 +48,20 @@ function findApple($name, $timeout = 60) {
     $devices = SQLSelect("SELECT appleIDs.APPLEID, appleIDs.PASSWORD, idevices.DEVICE_ID FROM appleIDs, idevices WHERE appleIDs.APPLEID = idevices.APPLEID AND idevices.NAME =  '".$name."'");
     foreach($devices as $device) {
       $FindMyiPhone = new FindMyiPhone($device['APPLEID'], $device['PASSWORD'], false);
-      //$FindMyiPhone->playSound($device['DEVICE_ID'], $subject);
+      $location =  $FindMyiPhone->locate($device['DEVICE_ID']);
+      $prop=SQLSelectOne("SELECT * FROM idevices WHERE APPLEID='".DBSafe($record['APPLEID'])."' AND DEVICE_ID='".DBSafe($device_id)."'");
+      $prop['NAME'] = $device->name;
+      $prop['DEVICE_ID'] = $device_id;
+      $prop['APPLEID'] = $record['APPLEID'];
+      $prop['BATTERY_LEVEL'] = $device->batteryLevel*100;
+      $prop['BATTERY_STATUS'] = ($device->batteryStatus == "NotCharging") ? 0 : 1;
+      if($location->horizontalAccuracy > 1000)
+        $location =  $FindMyiPhone->locate($device_id);
+      $prop['ACCURACY'] = $location->horizontalAccuracy;
+      $prop['LATITUDE'] = $location->latitude;
+      $prop['LONGITUDE'] = $location->longitude;
+      $prop['UPDATED']=date('Y-m-d H:i:s');
+      SQLUpdateInsert('idevices', $prop);
     }
   } catch (exception $e) {
     registerError('idevices', 'ERROR: ' . $e->getMessage());
